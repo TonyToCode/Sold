11e98356c6bbe9f01786a7d53a9198bfb5723853
TonyToCode <anton.ovsinskiy@russianschool.com>
2025-10-18 11:13:03 +0400

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,45 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true, // Для статического экспорта
+  },
+  output: 'export', // Включаем статический экспорт
+  trailingSlash: true, // Добавляем слеш в конце путей
+  distDir: 'dist', // Папка для сборки
+};
+
+export default nextConfig;
